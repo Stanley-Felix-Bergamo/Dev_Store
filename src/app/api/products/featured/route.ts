61dd94220ab47eb91e4cf675bace84dd110ad127
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-05-01 10:45:52 -0300

    commit

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,7 @@
 import data from '../data.json'
 
 export async function GET() {
+    await new Promise((resolve) => setTimeout(resolve, 1000)) // simulate delay
     const featuredProducts = data.products.filter(product => product.featured)
 
     return Response.json(featuredProducts)
