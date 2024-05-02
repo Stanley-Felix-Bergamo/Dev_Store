e2f2627c0017da43b3879a5237cc9937ff78b0ce
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-05-02 13:07:23 -0300

    commit

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from "@/components/add-to-cart-button";
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
 import { Metadata } from "next";
@@ -28,6 +29,15 @@ export async function generateMetadata({
   return {
     title: product.title,
   };
+}
+
+export async function generateStaticParams() {
+  const response = await api("/products/featured");
+  const products: Product[] = await response.json();
+
+  return products.map((product) => {
+    return { slug: product.slug };
+  });
 }
 
 export default async function ProductPage({ params }: ProductProps) {
@@ -61,6 +71,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </span>
 
           <span className="text-sm text-zinc-400">
+            Em até s/ juros de {""}
             {(product.price / 12).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL",
@@ -97,12 +108,8 @@ export default async function ProductPage({ params }: ProductProps) {
             </button>
           </div>
         </div>
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   );
